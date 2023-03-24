78f903279725ee75f58051f94bcd51ae43c3728e
Евгений Прутян <buivan.proger@gmail.com>
2023-03-24 16:57:39 +0200

    Added feature generated_diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,49 @@
 #!/usr/bin/env python3
 import argparse
+import json
 
 
-def main():
+def bool_lower(arg):
+    if arg is True:
+        return 'true'
+    elif arg is False:
+        return 'false'
+    else:
+        return arg
+
+
+def generate_diff(first_file, second_file):
+    result = '{'
+    data1 = json.load(open(first_file))
+    data2 = json.load(open(second_file))
+    sorted_data_keys = sorted(data1.keys() | data2.keys())
+    for key in sorted_data_keys:
+        if key in data1 and key in data2 and data1[key] == data2[key]:
+            result += f'\n    {key}: {bool_lower(data1[key])}'
+        elif key in data1 and key in data2 and data1[key] != data2[key]:
+            result += f'\n  - {key}: {bool_lower(data1[key])}'
+            result += f'\n  + {key}: {bool_lower(data2[key])}'
+        elif key not in data2:
+            result += f'\n  - {key}: {bool_lower(data1[key])}'
+        else:
+            result += f'\n  + {key}: {bool_lower(data2[key])}'
+    result += '\n}'
+    return result
+
+
+def start_parser():
     parser = argparse.ArgumentParser(
         description='Compares two configuration'
                     ' files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
-    parser.parse_args()
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
+
+
+def main():
+    start_parser()
 
 
 if __name__ == '__main__':

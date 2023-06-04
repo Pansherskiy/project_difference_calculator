3cd8e38a149f6796c95d00cac03fa03ebae6ecad
Евгений Прутян <buivan.proger@gmail.com>
2023-06-05 01:23:41 +0300

    error correction

--- difference_calculator/formatters/formatter.py
+++ difference_calculator/formatters/formatter.py
@@ -6,11 +6,11 @@ from difference_calculator.formatters.json import json
 def formatter(diff, style):
     match style.lower():
         case 'stylish':
-            return stylish(diff)
+            return stylish(diff).rstrip()
         case 'plain':
-            return plain(diff)
+            return plain(diff).rstrip()
         case 'json':
-            return json(diff)
+            return json(diff).rstrip()
         case _:
             print(f"Format '{style}' missing, choose among formats:"
                   f"\n'stylish'\n'plain'\n'json'")
